9a2305b0d34a1a00a713d691b1c35622b0a09681
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-06-13 13:39:36 +0545

    Implement routes for various events

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,5 +1,18 @@
 import express from "express";
+import {
+  getAllEvents,
+  getEventById,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+} from "../controllers/eventController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.get("/events", getAllEvents);
+router.get("/events/:id", getEventById);
+router.post("/events", authMiddleware, createEvent);
+router.put("/events/:id", authMiddleware, updateEvent);
+router.delete("/events/:id", authMiddleware, deleteEvent);
 export default router;
