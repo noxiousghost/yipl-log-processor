11c670ae9c97f93f7d20b697d814dfcd595dfb07
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-06-13 13:25:35 +0545

    Implement event model with JSON storage

---
+++ server/models/eventModel.js
@@ -0,0 +1,19 @@
+import fs from "fs-extra";
+import path from "path";
+
+const dataFilePath = path.resolve("src", "data", "events.json");
+
+fs.ensureFileSync(dataFilePath);
+
+export const readEvents = async () => {
+  try {
+    const data = await fs.readJson(dataFilePath);
+    return data;
+  } catch (err) {
+    return [];
+  }
+};
+
+export const writeEvents = async (events) => {
+  await fs.writeJson(dataFilePath, events, { spaces: 2 });
+};
