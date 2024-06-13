413b47b4e4259663b58a0b1821fcff9ec65ac747
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-06-13 14:03:15 +0545

    Implement CORS

--- server/app.js
+++ server/app.js
@@ -7,8 +7,14 @@ import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
+const corsOptions = {
+  origin: "http://localhost:3000", // Allow only the client on port 3000
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 // Enable CORS
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Parse JSON bodies
 app.use(bodyParser.json());
